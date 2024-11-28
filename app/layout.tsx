e5d01ef111588c13510a05af60bb0d67b74ee6ec
sasamile <nspes2020@gmail.com>
2024-11-28 11:07:04 -0500

    layout theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
         <SessionProvider session={session}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
